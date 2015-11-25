b7307a99eeec8455e8aa359537d9a5193dbe3d20
TangentJustin <justin@tangentsolutions.co.za>
2015-11-25 15:30:40 +0200

    composer package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started
+
+todo: write a readme file
---
+++ src/app/code/community/Clockworkgeek/Guestcookies/Test/Config/Module.php
@@ -0,0 +1,9 @@
+<?php
+class Clockworkgeek_Guestcookies_Test_Config_Module extends EcomDev_PHPUnit_Test_Case_Config
+{
+    public function testModuleParams()
+    {
+        $this->assertModuleCodePool("community");
+        $this->assertModuleVersion("1.0.0");
+    }
+}
